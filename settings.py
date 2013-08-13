1c5593fb618b896b1ee06d61e5cfcbc734604661
agarrow <alexio2@mac.com>
2013-08-13 16:34:56 -0400

    disregarding heroku setup instructions, ughhhhh

--- settings.py
+++ settings.py
@@ -27,8 +27,8 @@ DATABASES = {
 }
 
 # # Parse database configuration from $DATABASE_URL
-DATABASES = {}
-DATABASES['default'] =  dj_database_url.config()
+# DATABASES = {}
+# DATABASES['default'] =  dj_database_url.config()
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
