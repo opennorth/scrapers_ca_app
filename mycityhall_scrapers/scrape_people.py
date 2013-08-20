25893ea25d4d57ee94bb2c12b608d04e96197cb4
agarrow <alexio2@mac.com>
2013-08-20 16:00:24 -0400

    Revert "scrape_events script"
    This reverts commit a27d2cc17e575ac5c087d598f2bad74b8b91ea2c.
    
    going back to old scripts

--- mycityhall_scrapers/scrape_people.py
+++ mycityhall_scrapers/scrape_people.py
@@ -44,36 +44,3 @@ def scrape_people():
       report = {'name' : jurisdiction, 'status' : 'working' , 'error' : sys.exc_info()}
       reports.append(report)
   return reports
-
-def scrape_events():
-  jurisdictions = os.listdir('./mycityhall_scrapers')
-  scraper_path = os.path.abspath(os.path.join(os.path.abspath(__file__), os.pardir))+'/'
-  sys.path.insert(0,scraper_path)
-  jurisdiction = [x for x in jurisdictions if os.path.exists(scraper_path+x+'/events.py')]
-
-  reports = []
-
-  for jurisdiction in jurisdictions:
-    try:
-      COMMAND_MODULES = (
-          'pupa.cli.commands.update',
-      )
-      parser = argparse.ArgumentParser('pupa', description='pupa CLI')
-      subparsers = parser.add_subparsers(dest='subcommand')
-
-      subcommands = {}
-      for mod in COMMAND_MODULES:
-        cmd = importlib.import_module(mod).Command(subparsers)
-        subcommands[cmd.name] = cmd
-
-      # process args
-      args = parser.parse_args(['update', '--events', jurisdiction])
-      subcommands[args.subcommand].handle(args)
-
-      report = {'name' : jurisdiction, 'status' : 'working', 'error' : 'none' }
-      reports.append(report)
-
-    except:
-      report = {'name' : jurisdiction, 'status' : 'working' , 'error' : sys.exc_info()}
-      reports.append(report)
-  return reports
