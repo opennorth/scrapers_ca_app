b2c9276949ebe560ec9eb410936592c75e23efd1
agarrow <alexio2@mac.com>
2013-08-20 13:03:11 -0400

    mycityhall scrapers updated

--- scrape_people.py
+++ scrape_people.py
@@ -10,6 +10,6 @@ from reports.models import Report
 reports = scrape_people.scrape_people()
 Report.objects.all().delete()
 for report in reports:
-  r = Report(name=report.name, status=report.status, error=report.error)
+  r = Report(name=report['name'], status=report['status'], error=report['error'])
   r.save()
 
