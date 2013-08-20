e45acd05a7f4a799f2a2bc1b8e13f4cda2ebdfb0
agarrow <alexio2@mac.com>
2013-08-20 16:36:27 -0400

    fixed scripts

--- mycityhall_scrapers/scrape.py
+++ mycityhall_scrapers/scrape.py
@@ -44,3 +44,40 @@ def scrape_people():
       report = {'name' : jurisdiction, 'status' : 'working' , 'error' : sys.exc_info()}
       reports.append(report)
   return reports
+
+def scrape_events():
+  jurisdictions = os.listdir('./mycityhall_scrapers')
+  # scraper_path = os.path.abspath(os.path.join(os.path.abspath(__file__), os.pardir))+'/mycityhall_scrapers/'
+  scraper_path = os.path.abspath(os.path.join(os.path.abspath(__file__), os.pardir))+'/'
+  print scraper_path
+  sys.path.insert(0,scraper_path)
+  jurisdictions = [x for x in jurisdictions if os.path.exists(scraper_path+x+'/events.py')]
+
+
+  reports = []
+
+
+  for jurisdiction in jurisdictions:
+    try:
+      COMMAND_MODULES = (
+          'pupa.cli.commands.update',
+      )
+      parser = argparse.ArgumentParser('pupa', description='pupa CLI')
+      subparsers = parser.add_subparsers(dest='subcommand')
+
+      subcommands = {}
+      for mod in COMMAND_MODULES:
+        cmd = importlib.import_module(mod).Command(subparsers)
+        subcommands[cmd.name] = cmd
+
+      # process args
+      args = parser.parse_args(['update', '--events', jurisdiction])
+      subcommands[args.subcommand].handle(args)
+
+      report = {'name' : jurisdiction, 'status' : 'working', 'error' : 'none' }
+      reports.append(report)
+
+    except:
+      report = {'name' : jurisdiction, 'status' : 'working' , 'error' : sys.exc_info()}
+      reports.append(report)
+  return reports
--- scrape.py
+++ scrape.py
@@ -7,16 +7,16 @@ from pupa.cli import __main__
 from pupa.cli.commands import update, base
 from reports.models import Report
 
+print sys.argv
 
-if len(sys.argv) == 0:
+if len(sys.argv) == 1:
   reports = scrape.scrape_people()
   reports = reports + scrape.scrape_events()
 else:
-  if sys.argv[0] == 'people':
+  if sys.argv[1] == 'people':
     reports = scrape.scrape_people()
-  if sys.argv[0] == 'events':
+  if sys.argv[1] == 'events':
     reports = scrape.scrape_events()
-reports = scrape.scrape_people()
 Report.objects.all().delete()
 for report in reports:
   r = Report(name=report['name'], status=report['status'], error=report['error'])
