47cacb68ae86975782cb3b747553f0471c6befae
James McKinney <james@slashpoundbang.com>
2014-01-08 15:25:40 -0500

    small fixes

--- reports/models.py
+++ reports/models.py
@@ -3,5 +3,5 @@ from django.db import models
 # Create your models here.
 class Report(models.Model):
   name = models.TextField()
-  status = models.CharField(max_length=50)
-  error = models.TextField(max_length=1000)
+  status = models.TextField()
+  error = models.TextField()
