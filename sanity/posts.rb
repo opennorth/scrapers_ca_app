e997661945992983497dde2b36e92bbac2d7c903
James McKinney <james@slashpoundbang.com>
2014-02-12 15:05:20 -0500

    add all provinces to posts.js

--- sanity/posts.rb
+++ sanity/posts.rb
@@ -4,12 +4,10 @@ require 'open-uri'
 File.open('posts.js', 'w') do |f|
   f.write "var posts = {}\n"
 
-  { 'ocd-division/country:ca/province:bc' => 'province-bc-electoral_districts',
-    'ocd-division/country:ca/province:sk' => 'province-sk-electoral_districts',
-  }.each do |identifier,filename|
-    f.write %(posts['#{identifier}'] = []\n)
-    CSV.parse(open("https://raw.github.com/opencivicdata/ocd-division-ids/master/identifiers/country-ca/#{filename}.csv")) do |row|
-      f.write %(posts['#{identifier}'].push("#{row[1]}")\n)
+  %w(pe ns nb qc on mb sk ab bc).each do |type_id|
+    f.write %(posts['ocd-division/country:ca/province:#{type_id}'] = []\n)
+    CSV.parse(open("https://raw.github.com/opencivicdata/ocd-division-ids/master/identifiers/country-ca/province-#{type_id}-electoral_districts.csv")) do |row|
+      f.write %(posts['ocd-division/country:ca/province:#{type_id}'].push("#{row[1]}")\n)
     end
   end
 
