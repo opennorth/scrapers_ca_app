b8cc90b8a50e3ea9b520a1f00a48c81206d2a18a
James McKinney <james@slashpoundbang.com>
2014-02-13 23:58:44 -0500

    move some sanity scripts into validations

--- sanity/contact_details.js
+++ sanity/contact_details.js
@@ -1,37 +1,4 @@
 // @todo Move into models?
-
-matches('memberships', 'jurisdiction_id', {
-  contact_details: {
-    $elemMatch: {
-      type: 'email',
-      note: {$ne: null},
-    },
-  },
-}, 'memberships: email with non-empty note');
-
-matches('memberships', 'jurisdiction_id', {
-  contact_details: {
-    $elemMatch: {
-      type: {$ne: 'email'},
-      note: null,
-    },
-  },
-}, 'memberships: non-email with empty note');
-
-matches('memberships', 'jurisdiction_id', {
-  $where: function () {
-    var count = 0;
-    for (var i = 0, l = this.contact_details.length; i < l; i++) {
-      if (this.contact_details[i].type === 'email') {
-        count += 1;
-      }
-      if (count > 1) {
-        return true;
-      }
-    }
-  },
-}, 'memberships: multiple contact_details with the same type: email');
-
 matches('memberships', 'jurisdiction_id', {
   $where: function () {
     var types = ['address', 'cell', 'fax', 'voice'];
