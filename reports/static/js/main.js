125a9b30c4f7ce396a51576ead7fe2e91ab44df3
James McKinney <james@slashpoundbang.com>
2014-02-16 14:34:08 -0500

    try new table sorting textExtraction

--- reports/static/js/main.js
+++ reports/static/js/main.js
@@ -1,19 +1,4 @@
 $(function () {
-  $('.tablesorter').tablesorter({
-    textExtraction: function (node) {
-      if (node.textContent) {
-        return node.textContent;
-      } else {
-        if (node.childNodes[1] && node.childNodes[1].title) {
-          return node.childNodes[1].title;
-        }
-        else if (node.childNodes[0] && node.childNodes[0].hasChildNodes()) {
-          return node.childNodes[0].innerHTML;
-        } else {
-          return node.innerHTML;
-        }
-      }
-    }
-  });
+  $('.tablesorter').tablesorter();
   $('abbr.timeago').timeago();
 });
