227dfe4697e0ceed7b62c4b991be777622c2944b
Michael Mulley <michael@michaelmulley.com>
2014-08-25 13:24:19 -0400

    "constituency (2)" -> "constituency"

--- reports/utils.py
+++ reports/utils.py
@@ -13,7 +13,7 @@ def get_offices(obj):
   for contact_detail in obj['contact_details']:
     if contact_detail['type'] != 'email' and contact_detail['note']:
       note = contact_detail['note']
-      offices_by_note[note]['type'] = note
+      offices_by_note[note]['type'] = note.partition(' ')[0]
       offices_by_note[note][CONTACT_DETAIL_TYPE_MAP[contact_detail['type']]] = contact_detail['value']
   return list(offices_by_note.values())
 
