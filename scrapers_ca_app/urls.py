3254aebcd9e71a929cbd04afdd2672a0facab81f
James McKinney <james@slashpoundbang.com>
2014-10-13 11:58:03 -0400

    update requirements and change site configuration for Django 1.7

--- scrapers_ca_app/urls.py
+++ scrapers_ca_app/urls.py
@@ -1,19 +1,6 @@
-from django.conf.urls import patterns, include, url
-
-# Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.conf.urls import patterns, url
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'scrapers_ca_app.views.home', name='home'),
-    # url(r'^scrapers_ca_app/', include('scrapers_ca_app.foo.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
     url(r'^report/(?P<module_name>[a-z0-9_]+)/$', 'reports.views.report', name='report'),
     url(r'^represent/(?P<module_name>[a-z0-9_]+)/$', 'reports.views.represent', name='represent'),
     url(r'^warnings/$', 'reports.views.warnings', name='warnings'),
