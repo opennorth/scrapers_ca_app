f6b901f3d40958722d90e93c4645f1b3cec545f8
James McKinney <james@slashpoundbang.com>
2014-10-17 11:47:48 -0400

    py3: decode

--- reports/management/commands/upload.py
+++ reports/management/commands/upload.py
@@ -50,7 +50,7 @@ class UnicodeWriter:
         self.writer.writerow([s.encode("utf-8") if s else '' for s in row])
         # Fetch UTF-8 output from the queue ...
         data = self.queue.getvalue()
-        data = data.decode("utf-8")
+        data = data
         # ... and reencode it into the target encoding
         data = self.encoder.encode(data)
         # write to the target stream
