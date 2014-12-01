8b42f976c31f90b8eb1de42977ea6c2796f0937d
James McKinney <james@slashpoundbang.com>
2014-12-01 09:52:20 -0500

    flush with division_id not jurisdiction_id

--- reports/management/commands/flush.py
+++ reports/management/commands/flush.py
@@ -3,17 +3,19 @@ from opencivicdata.models import Jurisdiction, Person
 
 
 class Command(BaseCommand):
-    args = '<jurisdiction-id ...>'
+    args = '<division-id ...>'
     help = 'Deletes all documents from one jurisdiction'
 
     def handle(self, *args, **options):
-        for jurisdiction_id in args:
+        for division_id in args:
             try:
+                jurisdiction_id = '{}/{}'.format(division_id.replace('ocd-division', 'ocd-jurisdiction'), 'legislature')
                 qs = Person.objects.filter(memberships__organization__jurisdiction_id=jurisdiction_id)
-                print(qs.count())
+                people_count = qs.count()
                 qs.delete()
                 qs = Jurisdiction.objects.filter(id=jurisdiction_id)
-                print(qs.count())
+                jurisdiction_count = qs.count()
                 qs.delete()  # cascades everything except Person and Division
+                print("%s: %s people in %s jurisdiction" % (jurisdiction_id, people_count, jurisdiction_count))
             except Jurisdiction.DoesNotExist:
                 print("No Jurisdiction with id='%s'" % jurisdiction_id)
