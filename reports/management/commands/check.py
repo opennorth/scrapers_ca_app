31a4ca59cbcfa9d21a29bf4605a5f40e1773f388
James McKinney <james@slashpoundbang.com>
2015-10-04 20:35:24 -0400

    account for expected duplicates in some jurisdictions

--- reports/management/commands/check.py
+++ reports/management/commands/check.py
@@ -26,18 +26,24 @@ class Command(BaseCommand):
         if args:
             division_id = module_name_to_metadata(args[0])['division_id']
             jurisdictions = Jurisdiction.objects.filter(division_id=division_id)
-            organizations = Organization.objects.filter(jurisdiction__in=jurisdictions)
-            posts = Post.objects.filter(organization__in=organizations)
-            people = Person.objects.filter(memberships__organization__in=organizations)
-            memberships = Membership.objects.filter(person__id__in=people)
-            contact_details = MembershipContactDetail.objects.filter(membership__in=memberships)
         else:
-            jurisdictions = Jurisdiction.objects
-            organizations = Organization.objects
-            posts = Post.objects
-            people = Person.objects
-            memberships = Membership.objects
-            contact_details = MembershipContactDetail.objects
+            # Exclude candidate scrapers.
+            jurisdictions = Jurisdiction.objects.exclude(classification='executive')
+
+        organizations = Organization.objects.filter(jurisdiction__in=jurisdictions)
+        posts = Post.objects.filter(organization__in=organizations)
+        people = Person.objects.filter(memberships__organization__in=organizations)
+        memberships = Membership.objects.filter(person_id__in=people)
+        contact_details = MembershipContactDetail.objects.filter(membership__in=memberships)
+
+        jurisdiction_with_repetition = {
+            # Regions
+            'ocd-jurisdiction/country:ca/cd:3521/legislature': 4,
+            'ocd-jurisdiction/country:ca/cd:3526/legislature': 1,
+            'ocd-jurisdiction/country:ca/cd:3530/legislature': 1,
+            # Brampton
+            'ocd-jurisdiction/country:ca/csd:3521010/legislature': 4,
+        }
 
         post_memberships_count = posts.values('id').annotate(count=Count('memberships'))
 
@@ -65,23 +71,40 @@ class Command(BaseCommand):
         results = Counter(memberships.filter(post_id=None).exclude(organization__classification='party').values_list('organization__name', flat=True))
         self.report_count('non-party organizations have memberships with no posts', results)
 
-        # Validate that people have at most one post-membership and one party-membership.
-        results = people.values('id').exclude(memberships__organization__classification='party').annotate(count=Count('memberships')).exclude(count=1).values_list('name', flat=True)
+        # Validate that people have at most one post-membership.
+        results = people.values('id').exclude(memberships__organization__classification='party').exclude(memberships__organization__jurisdiction_id__in=jurisdiction_with_repetition.keys()).annotate(count=Count('memberships')).exclude(count=1).values_list('name', flat=True)
         self.report_value('people have many non-party memberships', results)
+        for jurisdiction_id, threshold in jurisdiction_with_repetition.items():
+            results = people.values('id').exclude(memberships__organization__classification='party').filter(memberships__organization__jurisdiction_id=jurisdiction_id).annotate(count=Count('memberships')).exclude(count__lte=threshold).values_list('name', flat=True)
+            self.report_value('people have many non-party memberships in {}'.format(jurisdiction_id), results)
+
+        # Validate that people have at most one party-membership.
         results = people.values('id').filter(memberships__organization__classification='party').annotate(count=Count('memberships')).exclude(count=1).values_list('name', flat=True)
         self.report_value('people have many party memberships', results)
 
-        # Validate the uniqueness of names, images and link URLs.
-        results = self.repeated(people.exclude(image='').values_list('image', flat=True))
+        # Validate the uniqueness of names and images.
+        people_without_repetition = people.exclude(memberships__organization__jurisdiction_id__in=jurisdiction_with_repetition.keys())
+        results = self.repeated(people_without_repetition.exclude(image='').values_list('image', flat=True))
         self.report_count('people have the same image', results)
-        results = self.repeated(people.values_list('name', flat=True))
+        results = self.repeated(people_without_repetition.values_list('name', flat=True))
         self.report_count('people have the same name', results)
+        for jurisdiction_id, threshold in jurisdiction_with_repetition.items():
+            people_with_repetition = people.filter(memberships__organization__jurisdiction_id=jurisdiction_id)
+            results = self.repeated(people_with_repetition.exclude(image='').values_list('image', flat=True), threshold=threshold)
+            self.report_count('people have the same image in {}'.format(jurisdiction_id), results)
+            results = self.repeated(people_with_repetition.values_list('name', flat=True), threshold=threshold)
+            self.report_count('people have the same name in {}'.format(jurisdiction_id), results)
+
+        # Validate the uniqueness of link URLs.
         results = self.repeated(people.exclude(links__url=None).values_list('links__url', flat=True))
         self.report_count('people have the same link URL', results)
 
         # Validate the uniqueness of email contact detail values.
-        results = self.repeated(contact_details.filter(type='email').values_list('value', flat=True))
+        results = self.repeated(contact_details.filter(type='email').exclude(membership__organization__jurisdiction_id__in=jurisdiction_with_repetition.keys()).values_list('value', flat=True))
         self.report_count('membership contact details with the same email', results)
+        for jurisdiction_id, threshold in jurisdiction_with_repetition.items():
+            results = self.repeated(contact_details.filter(type='email').filter(membership__organization__jurisdiction_id=jurisdiction_id).values_list('value', flat=True), threshold=threshold)
+            self.report_count('membership contact details with the same email in {}'.format(jurisdiction_id), results)
 
         # Validate presence of email contact detail.
         jurisdiction_with_no_email = [
@@ -119,8 +142,8 @@ class Command(BaseCommand):
                 if memberships_with_no_email > 1 or memberships_with_no_email and jurisdiction_id not in leaders_with_no_email:
                     log.error('{:2} memberships have no email in {}'.format(memberships_with_no_email, organization.name))
 
-    def repeated(self, results):
-        return {value: count for value, count in Counter(results).items() if count > 1}
+    def repeated(self, results, *, threshold=1):
+        return {value: count for value, count in Counter(results).items() if count > threshold}
 
     def report_value(self, message, results):
         if results:
@@ -133,5 +156,5 @@ class Command(BaseCommand):
         if results:
             log.error('{} {}:'.format(len(results), message))
             for value, count in sorted(results.items(), key=operator.itemgetter(1), reverse=True):
-                log.info('{} {}'.format(count, value))
+                log.info('{:2} {}'.format(count, value))
             log.info('---')
